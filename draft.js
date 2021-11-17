11123150d5ccc30eb731db0b1dde6401cfcff876
Hachim Youssoufa <feihachim@yahoo.fr>
2021-11-17 22:11:35 +0100

    First commit

---
+++ draft.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class Ball {
+    constructor(x, y, radius, speed, velocityX, velocityY, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed = speed;
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.color = color;
+    }
+
+
+}
